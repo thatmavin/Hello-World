b83d60d23327fad0f17ba6559e77fd2f07b8ef34
etwilsongit <etwilson.2012@gmail.com>
2017-06-09 11:13:13 -0400

    initial commit

---
+++ PracYo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracYo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hw = "Hello World";
+
+            Console.WriteLine("The first lesson will be learning " + hw);
+            
+            
+        }
+    }
+}
